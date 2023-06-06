2de0360aef21d966ccf68449408db17568515edc
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-06-06 19:20:28 +0530

    added landing, register, login page

---
+++ php/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/register.css"/>
+    <title>registration</title>
+</head>
+<body>
+    <div class="container">
+    <h1>Welcome!</h1>
+    <div>
+        <label for="username">Username</label>
+        <input type="text" id="username" autocomplete="off"/>
+
+        <label for="password">Password</label>
+        <input type="password" id="password" autocomplete="off"/>
+
+        <button>Join</button>
+    </div>
+    </div>
+</body>
+</html>
