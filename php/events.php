773141a0c687c4eeaf3b880a025916b298158915
Ishika Ranjan <ishikaranjan8@gmail.com>
2023-06-16 20:21:43 +0530

    added functionality

--- php/events.php
+++ php/events.php
@@ -16,53 +16,62 @@
         <h1>Events</h1>
 
         <?php
-// Database connection details
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'users';
+        // Database connection details
+        $host = 'localhost';
+        $username = 'root';
+        $password = '';
+        $database = 'users';
 
-// Establish a database connection
-$connection = mysqli_connect($host, $username, $password, $database);
+        // Establish a database connection
+        $connection = mysqli_connect($host, $username, $password, $database);
 
-// Check if the connection was successful
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+        // Check if the connection was successful
+        if (!$connection) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
 
-// Start or resume the session
-session_start();
+        // Start or resume the session
+        session_start();
 
-//Check if the user is already logged in
-if (!isset($_SESSION['username'])) {
-    // Redirect the user to the login page if not logged in
-    header("Location: login.php");
-    exit();
-}
+        // Check if the user is already logged in
+        if (!isset($_SESSION['username'])) {
+            // Redirect the user to the login page if not logged in
+            header("Location: login.php");
+            exit();
+        }
 
-// Include the data file
-include 'data.php';
+        // Include the data file
+        include 'data.php';
 
-// Check if the form is submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve the selected event from the form
-    $eventId = $_POST['event_id'];
+        // Check if the form is submitted
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Retrieve the selected event from the form
+            $eventId = $_POST['event_id'];
 
-    // Retrieve the username from the session
-    $username = $_SESSION['username'];
+            // Retrieve the username from the session
+            $username = $_SESSION['username'];
 
-    // Insert the event and user association into the database
-    $query = "INSERT INTO user_events(username, event_id) VALUES ('$username', '$eventId')";
-    $result = mysqli_query($connection, $query);
+            // Check if the user has already joined the event
+            $checkQuery = "SELECT * FROM user_events WHERE username = '$username' AND event_id = '$eventId'";
+            $checkResult = mysqli_query($connection, $checkQuery);
 
-    // Check if the query was successful
-    if ($result) {
-        echo "<h1 style='text-align:center; color:green'>Event joined successfully!</h1>";
-    } else {
-        echo "Error: " . mysqli_error($connection);
-    }
-}
-?>
+            if (mysqli_num_rows($checkResult) > 0) {
+                // User has already joined the event
+                echo "<h1 style='text-align:center; color:red'>Already registered for this event!</h1>";
+            } else {
+                // User hasn't joined the event yet, insert the data
+                $insertQuery = "INSERT INTO user_events(username, event_id) VALUES ('$username', '$eventId')";
+                $insertResult = mysqli_query($connection, $insertQuery);
+
+                // Check if the query was successful
+                if ($insertResult) {
+                    echo "<h1 style='text-align:center; color:green'>Event joined successfully!</h1>";
+                } else {
+                    echo "Error: " . mysqli_error($connection);
+                }
+            }
+        }
+        ?>
 
         <div>
             <?php
@@ -72,6 +81,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo '<div class="text-content">';
                 echo '<h2>'. $event['eventname'].'</h2>';
                 echo '<p>'. $event['description'] .'</p>';
+
+                // Retrieve users participating in the event
+                $eventId = $event['id'];
+                $usersQuery = "SELECT register.username, register.age, register.gender FROM register INNER JOIN user_events ON register.username = user_events.username WHERE user_events.event_id = '$eventId'";
+                $usersResult = mysqli_query($connection, $usersQuery);
+
+                echo '<h4>Participants:</h4>';
+                if (mysqli_num_rows($usersResult) > 0) {
+                    echo '<table style="border: 1px solid;  width: 100%; text-align:center">';
+                    echo '<thead>';
+                    echo '<tr><th style="border: 1px solid">Username</th><th style="border: 1px solid">Age</th><th style="border: 1px solid">Gender</th></tr>';
+                    echo '</thead>';
+                    echo '<tbody>';
+                    while ($row = mysqli_fetch_assoc($usersResult)) {
+                        echo '<tr>';
+                        echo '<td style="border: 1px solid">' . $row['username'] . '</td>';
+                        echo '<td style="border: 1px solid">' . $row['age'] . '</td>';
+                        echo '<td style="border: 1px solid">' . $row['gender'] . '</td>';
+                        echo '</tr>';
+                    }
+                    echo '</tbody>';
+                    echo '</table>';
+                } else {
+                    echo '<p>--</p>';
+                }
+
                 echo '<form method="post" action="">';
                 echo '<input type="hidden" name="event_id" value="' . $event['id'] . '">';
                 echo '<button type="submit">Join</button>';
